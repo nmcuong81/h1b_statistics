2bfc4d86bf2ec2c34ef942c3f55b426bbde9f363
Manh Cuong (MC) Nguyen <nmcuong81@gmail.com>
2018-10-25 17:39:54 -0700

    Minor corrections and adding README to src

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 For this problem, I first read the data and store to 2 dictionaries:
 
-   - occ_dict: key is the OCCUPATIONS, a combination of Standard Occupational Classification (SOC)_NAME and SOC_CODE, and value is a list consisting of [number of CERTIFIED application, number of total application].
+   - occ_dict: key is the OCCUPATIONS, a combination of Standard Occupational Classification (SOC)_NAME and SOC_CODE, and value is the number of CERTIFIED for that OCCUPATIONS.
 	
-   - state_dict: key is the STATE code (WORKSITE_STATE), and value is number of CERTIFIED applications.
+   - state_dict: key is the STATE code (WORKSITE_STATE), and value is number of CERTIFIED applications for that STATE.
 
-Dictionary is the optimal data structure choice for this task since the dictionary uses hash-table so accessing to a dictionary item is fast and scalable, at order of O(1).
+Dictionary is the optimal data structure choice for this task since the dictionary uses hash-table so accessing to a dictionary item is fast and scalable, at order of O(1). Together with those 2 dictionary, there is another variable n_certified to hold total number of certified applications.
 
-Next, I create a list for top OCCUPATIONS with each element is a list of [OCCUPATIONS, number of CERTIFIED applications, percentage of number of CERTIFIED applications wrt total number of CERTIFIED applications]. A smimilar list for top STATES with each element is a list of [STATE, number of CERTIFIED applications]. These 2 lists then will be sorted by 2 fields (NUMBER_CERTIFIED_APPLICATIONS and then TOP_OCCUPATIONS, NUMBER_CERTIFIED_APPLICATIONS and then TOP_STATES, respectively for top OCCUPATIONS and top STATES) (by Python sorted() function) and select only n_top=10 first elements to write out to output files.
+Next, I create a list for top OCCUPATIONS with each element is a list of [OCCUPATIONS, number of CERTIFIED applications for that OCCUPATIONS]. A smimilar list for top STATES with each element is a list of [STATE, number of CERTIFIED applications for that STATE]. These 2 lists then will be sorted by 2 fields (NUMBER_CERTIFIED_APPLICATIONS and then TOP_OCCUPATIONS, NUMBER_CERTIFIED_APPLICATIONS and then TOP_STATES, respectively for top OCCUPATIONS and top STATES) (by Python sorted() function) and select only n_top=10 first elements to write out to output files.
--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -12,24 +12,22 @@ with open(sys.argv[1]) as csvfin:
     iline=0
     for fields in fin:
         if iline == 0:
-            # Getting the fields' indexes
+            # Getting the fields' indexes. This is important since data from different year may have different format and/or columns
             soc_name_ind = fields.index('SOC_NAME')
             soc_code_ind = fields.index('SOC_CODE')
             state_ind = fields.index('WORKSITE_STATE')
+            status_ind = fields.index('CASE_STATUS')
             n_fields = len(fields)
         else:
-            #print(len(fields))
             if len(fields) == n_fields: # ignore line with missing data
                 # Occupation dictionary
-                occ_name = fields[soc_name_ind].strip().upper()+'__'+fields[soc_code_ind].strip().upper()
-                if occ_name not in occ_dict:
-                    occ_dict[occ_name] = [0,1]
-                else:
-                    occ_dict[occ_name][1] += 1
-                    
-                if fields[2].strip().upper() == 'CERTIFIED':
-                    occ_dict[occ_name][0] += 1
-                
+                if fields[status_ind].strip().upper() == 'CERTIFIED':
+                    occ_name = fields[soc_name_ind].strip().upper()+'__'+fields[soc_code_ind].strip().upper()
+                    if occ_name not in occ_dict:
+                        occ_dict[occ_name] = 1
+                    else:
+                        occ_dict[occ_name] += 1
+                #if fields[status_ind].strip().upper() == 'CERTIFIED':
                     # State dictionary
                     state = fields[state_ind].strip().upper()
                     if state not in state_dict:
@@ -43,21 +41,19 @@ with open(sys.argv[1]) as csvfin:
 top_occ = []
 for occ_name in occ_dict:
     name = occ_name.split('__')[0]
-    top_occ.append((name, occ_dict[occ_name][0], occ_dict[occ_name][0]*100/n_certified))
+    top_occ.append((name, occ_dict[occ_name]))
 top_occ = sorted(top_occ, key=lambda x: (-x[1], x[0]), reverse=False)[0:n_top] # limit to only n_top row
 
 top_state = list(state_dict.items())
 top_state = sorted(top_state, key=lambda x: (-x[1], x[0]), reverse=False)[0:n_top] # limit to only n_top row
 
 # Writing output top occupations
-#with open('../output/top_10_occupations.txt','w') as fout:
 with open(sys.argv[2],'w') as fout:
     fout.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
     for i in range(len(top_occ)):
-        fout.write(top_occ[i][0]+';'+str(top_occ[i][1])+';'+str(round(top_occ[i][2],1))+'%\n')
+        fout.write(top_occ[i][0]+';'+str(top_occ[i][1])+';'+str(round(top_occ[i][1]*100/n_certified,1))+'%\n')
 
 # Writing output top states
-#with open('../output/top_10_states.txt','w') as fout:
 with open(sys.argv[3],'w') as fout:
     fout.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
     for i in range(len(top_state)):
